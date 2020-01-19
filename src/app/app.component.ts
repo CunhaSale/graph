49bf8968c8e878509fca9b8a016fd31ff3cc2428
Gabriel Cunha <gabriel_mcunha@hotmail.com>
2020-01-19 01:45:00 -0300

    setting the data in the graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Chart } from 'chart.js';
 import { ServicoDadosService } from './services/servico-dados.service';
-import { Observable } from 'rxjs';
 import { States } from './models/states';
 import { Counties } from './models/counties';
 import { Beneficiaries } from './models/beneficiaries';
- 
+
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,11 +22,16 @@ export class AppComponent implements OnInit {
   counties$: Observable<Counties[]>;
   beneficiaries$: Observable<Beneficiaries[]>;
 
+  beneficiary$: Observable<Beneficiaries[]>;
+  countyLastMonths: string[] = [];
+  amount: string[] = [];
+  chart: Chart;
+
   constructor(private servicoDados: ServicoDadosService) { }
 
   ngOnInit() {
     // tslint:disable-next-line: no-unused-expression
-    new Chart(this.el.nativeElement, {
+    this.chart = new Chart(this.el.nativeElement, {
       type: 'line',
       data: {
         labels: [
@@ -34,29 +41,71 @@ export class AppComponent implements OnInit {
         ],
         datasets: [
           {
-            data: [85, 72, 86, 81, 84, 86, 104, 60, 62, 65, 41, 58],
+            data: this.countyLastMonths,
             borderColor: '#00AEFF',
             fill: false,
-            label: 'Estados'
+            label: 'Benefíciados',
           },
           {
-            data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
-            borderColor: '#FFCC00',
-            fill: false,
-            label: 'Municípios'
+            data: this.amount,
+            hidden: true,
+            label: 'label to remove'
           }
         ]
+      },
+      options: {
+        legend: {
+          labels: {
+            filter: (item) => (!item.text.includes('label to remove'))
+          }
+        },
+        tooltips: {
+          callbacks: {
+            title: (tooltipItem, data) => (data.labels[tooltipItem[0].index]),
+            label: (tooltipItem, data) => (`${data.datasets[0].data[tooltipItem.index]} Beneficiados`),
+            afterLabel: (tooltipItem, data) => (`Valor total: ${data.datasets[1].data[tooltipItem.index]}`)
+          },
+          backgroundColor: '#F9F9F9',
+          titleFontSize: 16,
+          titleFontColor: '#0066ff',
+          bodyFontColor: '#000',
+          bodyFontSize: 14,
+          displayColors: false,
+          borderWidth: .5,
+          borderColor: '#000'
+        }
       }
     });
 
     this.states$ = this.servicoDados.getStates();
     this.counties$ = this.servicoDados.getCounties(11);
     this.beneficiaries$ = this.servicoDados.getBeneficiaries(201906, 1100015);
-
-    console.log(this.statesEl.nativeElement.value);
   }
 
-  selectOption(e) {
+  selectState(e) {
     this.counties$ = this.servicoDados.getCounties(e);
+  }
+
+  selectCounty(e) {
+    const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    this.updateOptions();
+    months.forEach(value => {
+      this.servicoDados.getBeneficiaries(`2019${value}`, e)
+      .pipe(
+        tap(res => {
+          if (res[0]) {
+            this.countyLastMonths.push(res[0].quantidadeBeneficiados.toString());
+            this.amount.push(res[0].valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}).toString());
+          }
+        }),
+        tap(() => this.chart.update())
+      ).subscribe();
+    });
+  }
+
+  private updateOptions() {
+    this.chart.data.datasets[0].data = this.countyLastMonths = [];
+    this.chart.data.datasets[1].data = this.amount = [];
+    this.chart.update();
   }
 }
