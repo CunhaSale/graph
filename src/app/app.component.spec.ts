234fa26f7d637310786846d35dade319d0bc085f
Gabriel Cunha <gabriel_mcunha@hotmail.com>
2020-04-11 23:18:42 -0300

    code improvements

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './layout/header/header.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ServicoDadosService } from './services/servico-dados.service';
+import { tap } from 'rxjs/operators';
+import { State } from './models/state';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,25 +12,35 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        HeaderModule,
+        HttpClientModule
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  function setup() {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+    const servicoDadosService = fixture.debugElement.injector.get(ServicoDadosService);
+
+    return { fixture, component, servicoDadosService };
+  }
+
+  it('should create the app', () => {
+    const { component } = setup();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'challengeJ'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('challengeJ');
+  it(`should have as title 'Beneficiários Bolsa Família dos últimos 12 meses'`, () => {
+    const { component } = setup();
+    expect(component.title).toEqual('Beneficiários Bolsa Família dos últimos 12 meses');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const { fixture } = setup();
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('challengeJ app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Beneficiários Bolsa Família dos últimos 12 meses');
   });
 });
--- src/app/services/servico-dados.service.spec.ts
+++ src/app/services/servico-dados.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ServicoDadosService } from './servico-dados.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ServicoDadosService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: ServicoDadosService = TestBed.get(ServicoDadosService);
